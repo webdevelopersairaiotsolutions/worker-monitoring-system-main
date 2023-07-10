cb3ae43083a8501b7b9ed51bfb5595d6240538d4
Pujita Chakraborty <117858130+pujita789@users.noreply.github.com>
2023-07-10 11:45:21 +0530

    Add files via upload

---
+++ db/conn.js
@@ -0,0 +1,10 @@
+const  { connect } = require ('mongoose');
+
+const DB = process.env.DATABASE
+
+connect(DB,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(()=> console.log("DataBase Connected")).catch((errr)=>{
+    console.log(errr);
+})
